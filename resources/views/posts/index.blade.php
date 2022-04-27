bb398b5859a12513dfeee982d47509c0ba0c2043
pekokkuri <bokkuri1120@docomo.ne.jp>
2022-04-28 06:09:56 +0900

    トップページ

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Fashion</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <center><h1>Today's fashion</h1></center>
+        <div class='API'>
+            <h4>服装指数,カレンダー,天気予報</h4>
+        </div>
+        <div class='posts'>
+            <div class='post'>
+                <h2 class='title'>タイムライン</h2>
+                <p class='image'>image</p>
+            </div>
+        </div>
+    </body>
+</html>
