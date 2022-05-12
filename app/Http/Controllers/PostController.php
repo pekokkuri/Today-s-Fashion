a85e1be56a69ea25b62c958c38a16987bc6058ff
pekokkuri <bokkuri1120@docomo.ne.jp>
2022-05-12 20:07:54 +0900

    外部API

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,16 +4,40 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use Illuminate\Http\Request;
+use Storage;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-    return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
     }
     
     public function show(Post $post)
     {
-    return view('posts/show')->with(['post' => $post]);
+        return view('posts/show')->with(['post' => $post]);
+    }
+    
+    public function create()
+    {
+        return view('posts/create');
+    }
+    
+    public function store(Request $request, Post $post)
+    {
+        $post = new Post;
+        $form = $request->all();
+      
+
+        //s3アップロード開始
+        $image = $request->file('image');
+        // バケットの`myprefix`フォルダへアップロード
+        $path = Storage::disk('s3')->putFile('myprefix', $image, 'public');
+        // アップロードした画像のフルパスを取得
+        $post->image = Storage::disk('s3')->url($path);
+
+        $post->save();
+
+        return redirect('/posts/' . $post->id);
     }
 }
